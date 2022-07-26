1e5caa9ce286d2e7e9ed3ebf10587171ebc9eb11
Scott Sickles <scottsickles@gmail.com>
2022-07-26 16:37:38 -0400

    split out network and fix its contract

--- nullstone.tf
+++ nullstone.tf
@@ -13,7 +13,7 @@ resource "random_string" "resource_suffix" {
   length  = 5
   lower   = true
   upper   = false
-  number  = false
+  numeric = false
   special = false
 }
 
@@ -21,16 +21,4 @@ locals {
   tags          = data.ns_workspace.this.tags
   block_name    = data.ns_workspace.this.block_name
   resource_name = "${data.ns_workspace.this.block_ref}-${random_string.resource_suffix.result}"
-}
-
-data "ns_connection" "network" {
-  name = "network"
-  type = "network/aws"
-}
-
-locals {
-  env_name           = data.ns_workspace.this.env_name
-  vpc_id             = data.ns_connection.network.outputs.vpc_id
-  public_subnet_ids  = data.ns_connection.network.outputs.public_subnet_ids
-  private_subnet_ids = data.ns_connection.network.outputs.private_subnet_ids
 }
